77dfe9ac9f511ec3ca610fd0526855c47b5155eb
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-02-05 12:02:06 -0600

    Finished the Activity

--- public/index.js
+++ public/index.js
@@ -1,22 +1,25 @@
-function main(){
-    const countContainer = document.querySelector('#count-container');
-    const incrementButton = document.querySelector('#increment-button');
-    const decrementButton = document.querySelector('#decrement-button');
+async function main() {
+    const countContainer = document.querySelector("#count-container");
+    const incrementButton = document.querySelector("#increment-button");
+    const decrementButton = document.querySelector("#decrement-button");
 
-    let countValue = 0;
+    let response = await fetch("http://localhost:9001/counter");
+    let responseJson = await response.json();
+    let countValue = responseJson.value;
+    console.log(countValue);
 
-    function increment(){
+    function increment() {
         countValue++;
         countContainer.textContent = countValue;
     }
 
-    function decrement(){
+    function decrement() {
         countValue--;
         countContainer.textContent = countValue;
     }
 
-    incrementButton.addEventListener('click', increment);
-    decrementButton.addEventListener('click', decrement);
+    incrementButton.addEventListener("click", increment);
+    decrementButton.addEventListener("click", decrement);
     countContainer.textContent = countValue;
 }
-main()
+main();
